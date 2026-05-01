00da15315630ebe48a593f1630b0672722d259c8
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-05-01 20:07:25 +0100

    add logic of controller to service

---
+++ backend/src/products/product.service.ts
@@ -0,0 +1,82 @@
+import { BadRequestException, NotFoundException } from "@nestjs/common";
+import { CreateProductDto } from "./dtos/create-product.dto";
+import { UpdateProductDto } from "./dtos/update-product.dto";
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+}
+
+export class ProductService {
+  private data: Product[] = [
+    { id: 1, title: "book1", price: 10 },
+    { id: 2, title: "book2", price: 20 },
+    { id: 3, title: "book3", price: 30 },
+    { id: 4, title: "book4", price: 40 },
+    { id: 5, title: "book5", price: 50 },
+  ];
+
+  getAll(): CreateProductDto[] {
+    if (this.data.length <= 0) {
+      throw new NotFoundException("no product to show");
+    }
+    return this.data;
+  }
+
+  getOne(id: number): Product {
+    const product = this.data.find((p: Product) => {
+      return p.id === id;
+    });
+    if (!product) {
+      throw new NotFoundException("product not found");
+    }
+    return product;
+  }
+
+  createProduct(data: CreateProductDto): Product {
+    const newProduct: Product = {
+      id: this.data.length + 1,
+      title: data.title,
+      price: data.price,
+    };
+    this.data.push(newProduct);
+    return newProduct;
+  }
+
+  updateOne(id: number, data: UpdateProductDto): UpdateProductDto {
+    const updateProduct = this.data.find((p: Product) => {
+      return p.id === id;
+    });
+    if (!updateProduct) {
+      throw new NotFoundException("product not found");
+    }
+    if (!data) {
+      throw new BadRequestException("body is required");
+    }
+    if (data.title !== undefined) {
+      updateProduct.title = data.title;
+    }
+    if (data.price !== undefined) {
+      if (typeof data.price === "string") {
+        updateProduct.price = parseInt(data.price);
+      } else {
+        updateProduct.price = data.price;
+      }
+    }
+    return updateProduct;
+  }
+
+  deleteOne(id: number) {
+    const deleteProduct = this.data.find((p: Product) => {
+      return p.id === id;
+    });
+    if (!deleteProduct) {
+      throw new NotFoundException("product not found");
+    }
+    this.data.splice(this.data.indexOf(deleteProduct), 1);
+    return {
+      message: "delete product succesfully",
+    };
+  }
+}
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,10 +1,8 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   ParseIntPipe,
   Post,
@@ -12,40 +10,22 @@ import {
 } from "@nestjs/common";
 import { CreateProductDto } from "./dtos/create-product.dto";
 import { UpdateProductDto } from "./dtos/update-product.dto";
-
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-}
+import { ProductService } from "./product.service";
+import type { Product } from "./product.service";
 
 @Controller("api/products")
 export class ProductsController {
-  private data: Product[] = [
-    { id: 1, title: "book1", price: 10 },
-    { id: 2, title: "book2", price: 20 },
-    { id: 3, title: "book3", price: 30 },
-    { id: 4, title: "book4", price: 40 },
-    { id: 5, title: "book5", price: 50 },
-  ];
+  // ! Bad practice
+  private productService: ProductService = new ProductService();
 
   @Get()
   getAllProducts(): CreateProductDto[] {
-    if (this.data.length <= 0) {
-      throw new NotFoundException("no product to show");
-    }
-    return this.data;
+    return this.productService.getAll();
   }
 
   @Get(":id")
   getSingleProduct(@Param("id", ParseIntPipe) id: number): Product {
-    const product = this.data.find((p: Product) => {
-      return p.id === id;
-    });
-    if (!product) {
-      throw new NotFoundException("product not found");
-    }
-    return product;
+    return this.productService.getOne(id);
   }
 
   @Post()
@@ -53,13 +33,7 @@ export class ProductsController {
     @Body()
     body: CreateProductDto,
   ): Product {
-    const newProduct: Product = {
-      id: this.data.length + 1,
-      title: body.title,
-      price: body.price,
-    };
-    this.data.push(newProduct);
-    return newProduct;
+    return this.productService.createProduct(body);
   }
 
   @Put(":id")
@@ -67,40 +41,11 @@ export class ProductsController {
     @Param("id", ParseIntPipe) id: number,
     @Body() body: UpdateProductDto,
   ): UpdateProductDto {
-    const updateProduct = this.data.find((p: Product) => {
-      return p.id === id;
-    });
-    if (!updateProduct) {
-      throw new NotFoundException("product not found");
-    }
-    if (!body) {
-      throw new BadRequestException("body is required");
-    }
-    if (body.title !== undefined) {
-      updateProduct.title = body.title;
-      console.log("dkhal");
-    }
-    if (body.price !== undefined) {
-      if (typeof body.price === "string") {
-        updateProduct.price = parseInt(body.price);
-      } else {
-        updateProduct.price = body.price;
-      }
-    }
-    return updateProduct;
+    return this.productService.updateOne(id, body);
   }
 
   @Delete(":id")
   deleteProduct(@Param("id", ParseIntPipe) id: number) {
-    const deleteProduct = this.data.find((p: Product) => {
-      return p.id === id;
-    });
-    if (!deleteProduct) {
-      throw new NotFoundException("product not found");
-    }
-    this.data.splice(this.data.indexOf(deleteProduct), 1);
-    return {
-      message: "delete product succesfully",
-    };
+    this.productService.deleteOne(id);
   }
 }
