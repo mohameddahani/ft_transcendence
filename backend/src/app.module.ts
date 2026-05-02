6724d71e168ed20884906095ff0e1749b82cbd2f
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-05-02 21:00:29 +0100

    try to connect with database and try to create an entity (table on database)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
 import { ProductsModule } from "./products/products.module";
 import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      username: "admin",
+      password: "1234",
+      port: 5432,
+      host: "localhost",
+      database: "ft_transcendence",
+      synchronize: true, // ! Only for development
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
