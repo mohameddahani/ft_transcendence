34d61b042335c27c0f4020e4b1f301573cf7c54c
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-05-03 19:06:19 +0100

    add all info of database to env file and create mode (developemt or production) as envirement var in package.json exact in script npm run start:dev

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ProductsModule } from "./products/products.module";
 import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Product } from "./products/product.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 // todo @Entity → define table
 // todo forRoot → register table in DB
@@ -13,15 +14,28 @@ import { Product } from "./products/product.entity";
   imports: [
     ProductsModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      username: "admin",
-      password: "1234",
-      port: 5432,
-      host: "localhost",
-      database: "ft_transcendence",
-      synchronize: true, // ! Only for development
-      entities: [Product],
+    // * if you want to use a .env file to manage environment variables, use ConfigModule.forRoot()
+    // * isGlobal: true → makes ConfigModule available globally across the whole application (no need to import in other modules)
+    // * envFilePath: path of .env file
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    // * connect to database using typeORM and get values from .env file using ConfigService
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: config.get<string>("DB_TYPE") as "postgres",
+          username: config.get<string>("DB_USERNAME"),
+          password: config.get<string>("DB_PASSWORD"),
+          port: config.get<number>("DB_PORT"),
+          host: config.get<string>("DB_HOST"),
+          database: config.get<string>("DB_DATABASE_NAME"),
+          synchronize: config.get<string>("NODE_ENV") === "development", // ! only for development mode (this condition will be true if i use env of development)
+          entities: [Product],
+        };
+      },
     }),
   ],
   controllers: [],
