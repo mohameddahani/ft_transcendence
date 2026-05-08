fadaa506e902871ddc9a3170694cf84e42273958
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-05-08 18:56:32 +0100

    refactor service of user (add logic of register and login in auth.provider.ts)

---
+++ backend/src/users/providers/auth.provider.ts
@@ -0,0 +1,105 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { RegisterUserDto } from '../dtos/register-user.dto';
+import { PrismaService } from '@/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { JWTPayload } from '@/utils/types';
+import { LoginUserDto } from '../dtos/login-user.dto';
+import { AccountStatus } from '@/generated/prisma/enums';
+import { JwtService } from '@nestjs/jwt';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+@Injectable()
+export class AuthProvider {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+  // * Register
+  async register(data: RegisterUserDto) {
+    // * Check if user already exist before register
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [
+          { userName: data.userName },
+          { email: data.email },
+          { phoneNumber: data.phoneNumber },
+        ],
+      },
+    });
+
+    if (existingUser) {
+      if (existingUser.userName === data.userName) {
+        throw new UnauthorizedException('Username already exists');
+      }
+
+      if (existingUser.email === data.email) {
+        throw new UnauthorizedException('Email already exists');
+      }
+
+      if (existingUser.phoneNumber === data.phoneNumber) {
+        throw new UnauthorizedException('Phone number already exists');
+      }
+    }
+
+    // * Hash the password
+    const salt = await bcrypt.genSalt(10);
+    data.password = await bcrypt.hash(data.password, salt);
+
+    // * Add user to database
+    const newUser = await this.prisma.user.create({ data });
+
+    // * Generate JWT
+    const payload: JWTPayload = { id: newUser.id, userType: newUser.userType };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+    // * Exclude Some Fields
+    const { id, password, createdAt, updatedAt, ...safeUser } = newUser;
+
+    return { newUser: safeUser, accessToken };
+  }
+
+  // * Login
+  async login(data: LoginUserDto) {
+    // * Check if user already exist by email before login
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid Email or Password');
+    }
+
+    // * Check status of account
+    if (user.accountStatus === AccountStatus.pending) {
+      throw new UnauthorizedException(
+        'Your account is pending verification. Please verify your email to continue.',
+      );
+    }
+
+    if (user.accountStatus === AccountStatus.inactive) {
+      throw new UnauthorizedException(
+        'Your account is inactive. Please activate your account through the email we sent.',
+      );
+    }
+
+    if (user.accountStatus === AccountStatus.banned) {
+      throw new UnauthorizedException(
+        'Your account has been suspended. Please contact support for assistance.',
+      );
+    }
+
+    // * Check the password is match
+    const passwordIsMatch = await bcrypt.compare(data.password, user.password);
+    if (!passwordIsMatch) {
+      throw new UnauthorizedException('Invalid Email or Password');
+    }
+
+    // * Generate JWT
+    const payload: JWTPayload = { id: user.id, userType: user.userType };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    // * Exclude Some Fields
+    const { id, password, createdAt, updatedAt, ...safeUser } = user;
+
+    return { user: safeUser, accessToken };
+  }
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthProvider } from './providers/auth.provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthProvider],
   imports: [],
   exports: [],
 })
