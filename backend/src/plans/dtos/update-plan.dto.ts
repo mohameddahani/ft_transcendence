2b0e660a1057d4dfa2785363e94168e3e874c912
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-07-02 13:37:08 +0100

    feat: add owner to update status of plan (active or not)

--- backend/src/plans/dtos/update-plan.dto.ts
+++ backend/src/plans/dtos/update-plan.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { AddPlanDto } from './add-plan.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePlanDto extends PartialType(AddPlanDto) {}
+export class UpdatePlanDto extends PartialType(AddPlanDto) {
+  // * Is Active
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
