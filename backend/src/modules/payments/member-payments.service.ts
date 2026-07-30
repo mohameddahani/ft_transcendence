a82315a90865645baa79cd8a41ca08b37edcf9a5
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-07-30 17:31:08 +0100

    fix payments

--- backend/src/modules/payments/member-payments.service.ts
+++ backend/src/modules/payments/member-payments.service.ts
@@ -66,7 +66,7 @@ export class MemberPaymentsService {
     // * Check if member has membership
     await this.checkIfMemberHasMembership(memberId);
 
-    const payment = await this.prisma.payment.findFirst({
+    const payment = await this.prisma.payment.findUnique({
       where: {
         id: id,
         memberId: memberId,
