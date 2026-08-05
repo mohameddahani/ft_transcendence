b7ebac095b752380bda1d63b33936f869f4f9874
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-08-05 14:38:18 +0100

    feat: add documentation swagger to dto of subscriptions module

--- backend/src/modules/platform/subscriptions/dtos/active-subscription.dto.ts
+++ backend/src/modules/platform/subscriptions/dtos/active-subscription.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsString, IsUUID, MaxLength, MinLength } from 'class-validator';
 
 export class ActiveSubscriptionDto {
   // * PlanId
   @IsUUID()
+  @ApiProperty({
+    example: 'b4453677-b504-44ec-890f-0f42710cd255',
+    description: 'Plan id',
+  }) // * Swagger Document
   planId!: string;
 
   // * DurationId
   @IsUUID()
+  @ApiProperty({
+    example: 'b4453677-b504-44ec-890f-0f42710cd151',
+    description: 'Plan Duration id',
+  }) // * Swagger Document
   planDurationId!: string;
 
   // * Username
@@ -18,5 +27,9 @@ export class ActiveSubscriptionDto {
   @Transform(({ value }): string =>
     typeof value === 'string' ? value.trim() : value,
   )
+  @ApiProperty({
+    example: 'zakaria-khaldoune-xyz',
+    description: 'User Name of Admin',
+  }) // * Swagger Document
   userName!: string;
 }
--- backend/src/modules/platform/subscriptions/dtos/cancel-subscription.dto.ts
+++ backend/src/modules/platform/subscriptions/dtos/cancel-subscription.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 
 export class CancelSubscriptionDto {
   // * AdminId
   @IsUUID()
+  @ApiProperty({
+    example: 'b4453677-b504-44ec-890f-0f42710cd355',
+    description: 'Admin id',
+  }) // * Swagger Document
   adminId!: string;
 }
